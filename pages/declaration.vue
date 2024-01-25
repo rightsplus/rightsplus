ef4dc3b8e13f510842a1076d113034226e6174f4
Leon Vogler <leonfvogler@gmail.com>
2024-01-25 12:26:42 +0000

    add declaration

---
+++ pages/declaration.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="relative overflow-hidden bg-white">
+    <div class="flex justify-start items-start absolute left-0 top-0 gap-6 p-9">
+      <div
+        class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-9"
+      >
+        <p
+          class="flex-grow-0 flex-shrink-0 text-2xl font-bold text-left text-black"
+        >
+          Abtretungserklärung
+        </p>
+        <div
+          class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-9"
+        >
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+          >
+            <span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >Ich,</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >Leon Vogler,</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >Gailhöfe 6,</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >88699 Frickingen</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >erkläre hiermit, dass ich meinen Anspruch auf Entschädigung gemäß
+              der Verordnung (EG) Nr. 261/2004 in Bezug auf den Flug </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >DE2184</span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+            >
+              am </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >26.10.2019</span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+            >
+              von </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >Madrid (MAD)</span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+            >
+              nach </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >Frankfurt am Main (FRA) </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >hiermit an </span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] font-bold text-left text-black"
+              >RightsPlus GbR, Sandeldamm 24a, 63450 Hanau</span
+            ><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+            >
+              unbedingt und unwiderruflich abtrete.</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >Der Abtretende erhält für seinen Anspruch eine Zahlung i.H.v
+              278,00 €, gemäß den aktuell gültigen AGB sowie Preis- und
+              Leistungsverzeichnis.</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >Der Abtretende erklärt zum Zeitpunkt der Abtretung
+              Anspruchsinhaber zu sein.</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >Die RightsPlus GbR, vertreten durch den geschäftsführenden
+              Gesellschafter Joachim Bawa erklärt hiermit die Annahme der
+              Abtretung.</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[13px] text-left text-black"
+              >N291A</span
+            >
+          </p>
+          <div
+            class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 gap-[5px]"
+          >
+            <div
+              class="flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 gap-3"
+            >
+              <div
+                class="flex justify-center items-center flex-grow w-[187px] h-[38px] gap-2.5 p-2.5 rounded-[5px] bg-[#f7f7f7]"
+              ></div>
+              <div
+                class="flex justify-center items-center flex-grow w-[187px] h-[38px] gap-2.5 p-2.5 rounded-[5px] bg-[#f7f7f7]"
+              ></div>
+            </div>
+            <div
+              class="flex justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-3"
+            >
+              <p class="flex-grow w-[187px] text-[10px] text-left text-black">
+                Ort, Datum
+              </p>
+              <p class="flex-grow w-[187px] text-[10px] text-left text-black">
+                Unterschrift
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 gap-6"
+      >
+        <div
+          class="flex justify-start items-center flex-grow-0 flex-shrink-0 relative gap-[5px]"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="flex-grow-0 flex-shrink-0 w-6 h-6 relative"
+            preserveAspectRatio="xMidYMid meet"
+          >
+            <rect width="24" height="24" rx="4" fill="#303B41"></rect>
+            <rect
+              x="4.66666"
+              y="4.66667"
+              width="14.6667"
+              height="2.66667"
+              rx="1"
+              fill="white"
+            ></rect>
+            <rect
+              x="4.66666"
+              y="10.6667"
+              width="14.6667"
+              height="2.66667"
+              rx="1"
+              fill="#F17C46"
+            ></rect>
+          </svg>
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[10px] font-bold text-left text-[#303b41]"
+          >
+            RightsPlus
+          </p>
+        </div>
+        <div
+          class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-3"
+        >
+          <div
+            class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0"
+          >
+            <div
+              class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[3px]"
+            >
+              <p
+                class="flex-grow-0 flex-shrink-0 text-[8px] text-left text-[#808080]"
+              >
+                Datum
+              </p>
+              <p
+                class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >
+                25.01.2024
+              </p>
+            </div>
+          </div>
+          <div
+            class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0"
+          >
+            <div
+              class="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[3px]"
+            >
+              <p
+                class="flex-grow-0 flex-shrink-0 text-[8px] text-left text-[#808080]"
+              >
+                Fallnummer
+              </p>
+              <p
+                class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >
+                RP7123923791
+              </p>
+            </div>
+          </div>
+          <p
+            class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+          >
+            <span
+              class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >RightsPlus GbR</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >Sandeldamm 24a</span
+            ><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >63450 Hanau</span
+            ><br /><br /><span
+              class="flex-grow-0 flex-shrink-0 text-[10px] text-left text-[#303b41]"
+              >info@rightsplus.de</span
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
