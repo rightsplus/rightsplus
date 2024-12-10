78f79e3ae1f658f0a4c3d50cf55c41242ad91de9
Leon Vogler <leonfvogler@gmail.com>
2024-12-10 11:32:24 +0100

    fix: remove padding

--- layouts/claims.vue
+++ layouts/claims.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col">
     <Header class="w-full z-50" />
     <div
-      class="min-h-screen p-12 bg-neutral-200 mx-auto w-full block md:flex gap-8"
+      class="min-h-screen md:p-12 bg-neutral-200 mx-auto w-full block md:flex gap-8"
     >
       <!-- <CellsSidebarMenu :user="user" :items="menu" /> -->
       <main class="w-full" :class="$route.meta.classes?.main">
