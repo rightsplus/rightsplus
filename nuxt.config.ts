47af4c245ef1cc484b0c44612946f73ac0393f22
Leon Vogler <leonfvogler@gmail.com>
2024-12-12 16:37:00 +0100

    fix: prerender admin and claim routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -142,40 +142,28 @@ export default defineNuxtConfig({
     }
   },
   routeRules: {
-    '/en/pdf/**': {
-      prerender: false,
-      ssr: false
-    },
-    '/de/pdf/**': {
-      prerender: false,
-      ssr: false
-    },
-    '/pdf/**': {
-      prerender: false,
-      ssr: false
-    },
     '/en/admin/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
     '/de/admin/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
     '/admin/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
     '/en/claim/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
     '/de/anspruch/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
     '/anspruch/**': {
-      prerender: false,
+      prerender: true,
       ssr: false
     },
   },
